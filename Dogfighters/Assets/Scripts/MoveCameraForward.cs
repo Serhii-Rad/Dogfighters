7f92aa62d1df4344cd73dcda48b7e543ca2285ce
serhii <radchucksergey@gmail.com>
2022-12-17 11:02:06 +0000

    camera end point

--- Dogfighters/Assets/Scripts/MoveCameraForward.cs
+++ Dogfighters/Assets/Scripts/MoveCameraForward.cs
@@ -3,23 +3,22 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 
-public class MoveForward : MonoBehaviour
+public class MoveCameraForward : MonoBehaviour
 {
-    [SerializeField]
-    private float _speed = 5f;
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private Transform _stopPoint;
 
     void Start()
     {
+        
     }
 
     void Update()
     {
-        Move();
+        if (_stopPoint.position.y > transform.position.y)
+            Move();
     }
 
     
-    private void Move()
-    {
-        transform.Translate(Vector2.up * _speed * Time.deltaTime);
-    }
+    private void Move() => transform.Translate(Vector2.up * _speed * Time.deltaTime);
 }
