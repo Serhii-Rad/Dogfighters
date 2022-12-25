61036d7fddfe0335b5351ef85b5ff0fbbd2448d5
serhii <radchucksergey@gmail.com>
2022-12-25 14:31:34 +0000

    Finish level method

--- Dogfighters/Assets/Scripts/Games Control Scripts/GameControl.cs
+++ Dogfighters/Assets/Scripts/Games Control Scripts/GameControl.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameControl : MonoBehaviour
 {
     [SerializeField] private GameObject _pausePanel;
+    [SerializeField] private GameObject _winScreen;
+    [SerializeField] private GameObject _lastEnemy;
+
     private bool _paused = false;
 
     void Start()
@@ -27,6 +31,7 @@ public class GameControl : MonoBehaviour
             _pausePanel.SetActive(!_paused);
             _paused = !_paused;
         }
+        LevelFinish();
     }
     public static void StartGame()
     {
@@ -38,6 +43,15 @@ public class GameControl : MonoBehaviour
     public static void PauseGame() => Time.timeScale = 0;
 
     public static void UnPauseGame() => Time.timeScale = 1;
+
+    public void LevelFinish()
+    {
+        if (_lastEnemy.IsDestroyed())
+        {
+            _winScreen.SetActive(true);
+            PauseGame();
+        }
+    }
 }
 
 public enum Scenes
